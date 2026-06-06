5118e0eebc9975f9457f1ba87f4d9992646cec6c
Suryansh Gupta <suryanshgupta160@gmail.com>
2026-06-06 10:29:56 +0530

    added local host  domain to cors

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ connectToDB().then(() => {
 
 // very important middleware
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  })
+)
 
 app.use('/api/auth', authRoutes)
 app.use('/api', adminRoutes)
